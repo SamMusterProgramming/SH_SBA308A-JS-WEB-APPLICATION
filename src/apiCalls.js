1d8fe7ae496e6a7d25c861072c75e6fdbb2f97b7
Samir Haddadi <sam20232022@gmail.com>
2024-11-04 19:24:59 +0000

    second commit

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 
-const API_KEY = 'uzspfFOdKNoTzjnSVZlDg3GNHFU8FBAVMcxbB1Ms'
-const baseURL = ` https://api.nasa.gov/planetary/earth/assets/&api_key=${API_KEY}`;
+
+const baseURL = "https://vigilant-space-parakeet-97j7wxg4pvj5cr6v-8080.app.github.dev";
 
 const axiosApiUrl = axios.create({
   baseURL:baseURL
 });
 
-export async function getUpdate(){
-   return await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?limit=20&sol=1000&api_key=${API_KEY
-    }`)
+export async function getPosts(){
+   return await axios.get("http://localhost:8080/posts")
     .then(res => res.data)
     
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getUpdate } from "./apiCalls";
+import { getPosts } from "./apiCalls";
 import {setUpTopBar} from "./navbar/nav"
 import { setUpSideBar, sideBar } from "./sideBar/sideBar";
 
@@ -7,22 +7,8 @@ setUpTopBar();
 
  const container = document.querySelector('#container')
  
- getUpdate().then((data) => { console.log(data.photos)
-    const photos = data.photos;
-    photos.map((photo) =>{
-        const div = document.createElement('div')
-        div.style.width = '10%'
-        div.style.height = '20%'
-        const img = document.querySelector('#img')
-        img.src = photo.img_src
-        div.appendChild(img)
-        container.appendChild(div)
-    })
-    
-
- }
-    
-)
+getPosts().then((data) =>  console.log(data))
+ 
     
 // 
 
